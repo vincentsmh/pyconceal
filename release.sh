ea1368567260769b785f5405b5d6d5d8c9432fc5
Vincent Huang <vincent.sm.huang@gmail.com>
2017-07-25 00:25:10 +0800

    Add release version

--- release.sh
+++ release.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-release_folder="pyconceal${version}"
+function get_version()
+{
+  git tag | head -n 1
+}
+
+version="$(get_version)"
+release_folder="pyconceal-${version}"
+rm -rf ${release_folder}
 mkdir -p ${release_folder}
 cp obfuscator.config dist/obfuscator ${release_folder}/
