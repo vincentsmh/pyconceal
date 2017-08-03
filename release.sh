87ce8199ecab65dd20960392a53a8013de807dc4
Vincent Huang <vincent.sm.huang@gmail.com>
2017-08-03 15:19:06 +0800

    Fix message for release

--- release.sh
+++ release.sh
@@ -2,7 +2,7 @@
 
 function get_version()
 {
-  git tag | head -n 1
+  git tag | tail -n 1
 }
 
 version="$(get_version)"
@@ -10,4 +10,7 @@ release_folder="pyconceal-${version}"
 rm -rf ${release_folder}
 mkdir -p ${release_folder}
 cp obfuscator.config dist/obfuscator doc/README ${release_folder}/
-tar zcvf ${release_folder}.tar.gz ${release_folder}
+tar zcvf ${release_folder}.tar.gz ${release_folder} > /dev/null
+rm -rf ${release_folder} build dist
+echo -e "Release package: ${release_folder}.tar.gz"
+echo -e
